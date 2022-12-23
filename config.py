2666c128ec9259c4ddbabcc3b9a88ebdbfa2f1ce
Ilya <tovbinim@gmail.com>
2022-12-23 20:19:11 +0300

    diplom

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = '' #токен пользователя
+comm_token = '' #токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) #перебор найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '123qweasdzxc'
+db_name = 'postgres'
